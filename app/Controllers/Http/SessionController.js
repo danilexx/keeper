888cfb5d508421eb8f67683c391242bd06802508
danilexx <42390775+danilexx@users.noreply.github.com>
2020-01-16 09:33:21 -0300

    changes

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,12 +1,18 @@
 'use strict'
 
-const User = use('App/Models/User')
-
 class SessionController {
   async store ({ request, auth }) {
     const { username, password } = request.all()
-    const token = await auth.attempt(username, password)
+    const token = await auth.withRefreshToken().attempt(username, password, true)
     return token
+  }
+
+  async update ({ request, auth }) {
+    const refreshToken = request.input('refresh_token')
+
+    const newJwtToken = await auth.generateForRefreshToken(refreshToken, true)
+
+    return newJwtToken
   }
 }
 
